bb5224c8e55e0616bea3928e056ddbbc882aa74b
agustina scarpinelli <agustina.scarpinelli@gmail.com>
2022-09-06 10:39:17 -0300

    readme

--- src/Components/PayForm/PayForm.js
+++ src/Components/PayForm/PayForm.js
@@ -1,126 +1,176 @@
-import './PayForm.css'
-import React, { useState } from 'react'
-import visa from '../../img/visa.svg'
-import amex from '../../img/amex.svg'
-import diners from '../../img/diners.svg'
-import master from '../../img/mastercard.svg'
-import creditcard from '../../img/credit-card-solid.svg'
-import {useNavigate} from 'react-router-dom'
-import { 
-  isValid, 
-  isExpirationDateValid, 
-  isSecurityCodeValid, 
-  getCreditCardNameByNumber 
-} from 'creditcard.js'
-import Swal from 'sweetalert2'
-
-
+import "./PayForm.css";
+import React, { useContext, useState } from "react";
+import visa from "../../img/visa.svg";
+import amex from "../../img/amex.svg";
+import diners from "../../img/diners.svg";
+import master from "../../img/mastercard.svg";
+import creditcard from "../../img/credit-card-solid.svg";
+import { useNavigate } from "react-router-dom";
+import {
+  isValid,
+  isExpirationDateValid,
+  isSecurityCodeValid,
+  getCreditCardNameByNumber,
+} from "creditcard.js";
+import Swal from "sweetalert2";
+import InputMask from "react-input-mask";
+import { useSelector } from "react-redux";
 
 export const PayForm = () => {
-const [success, setSuccess]=useState("")
-const [error,setError]=useState("")
-const [creditCard/*,setCreditCard*/]=useState("")
-const[creditLogo,setCreditLogo]=useState("")
-const[errorMsg,setErrorMsg]=useState("")
-const[creditNumber,setCreditNumber]=useState(null)
-const[expiration,setExpiration]=useState("")
-const[cvc,setCvc]=useState(null)
-const history=useNavigate()
+  const [error, setError] = useState("");
+  const [creditCard, setCreditCard] = useState("");
+  const [creditLogo, setCreditLogo] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+  const [creditNumber, setCreditNumber] = useState(null);
+  const [expiration, setExpiration] = useState("");
+  const [cvc, setCvc] = useState(null);
+  const history = useNavigate();
+  const { productSelected } = useSelector((state) => state.product.value);
+  const createOrder = () => {
+    const date = new Date();
+    const time = date.getTime();
+    const order = {
+      time: time,
+      date: date,
+      totalUSD:'productSelected.price',
+      totaBtc:,
+      product: {
+        name: 'productSelected.name',
+        id: 'productSelected.id',
+      },
+      creditCardData: {
+        number: creditNumber,
+        company: creditCard,
+      },
+    };
+    return order;
+  };
 
+  const validateCreditCardNumber = (value) => {
+    let card = getCreditCardNameByNumber(value);
+    setCreditCard(card);
+    setCreditNumber(value);
+    switch (card) {
+      case "Visa":
+        setCreditLogo(visa);
+        setErrorMsg("");
+        break;
+      case "Mastercard":
+        setCreditLogo(master);
+        setErrorMsg("");
 
-const validateCreditCardNumber=(value)=>{
+        break;
+      case "Diners":
+        setCreditLogo(diners);
+        setErrorMsg("");
 
-  let card= getCreditCardNameByNumber(value)
-  console.log(card)
-  setCreditNumber(value)
-  switch(card){
-    case "Visa": setCreditLogo(visa)
-    setErrorMsg("")
-    break;
-    case "Mastercard": setCreditLogo(master)
-    setErrorMsg("")
-    
-    break;
-    case "Diners":setCreditLogo(diners)
-    setErrorMsg("")
-  
-      break;
-    case "American Express":setCreditLogo(amex)
-    setErrorMsg("")
+        break;
+      case "American Express":
+        setCreditLogo(amex);
+        setErrorMsg("");
 
-      break;
-    default: setCreditLogo(creditcard)
-    
-      setErrorMsg("Please,enter a valid credit card number")
-      break;
-  }
-if ((card==="Visa" || card==="Mastercard" || card==="Diners" ||  card==="American Express" ) && (isValid(value))){
-    setCreditNumber(value)
-    return true
-}
-else{
-  setErrorMsg("Please, enter a valid credit card number")
-  return false
-}
-}
+        break;
+      default:
+        setCreditLogo(creditcard);
+        break;
+    }
+    if (
+      (card === "Visa" ||
+        card === "Mastercard" ||
+        card === "Diners" ||
+        card === "American Express") &&
+      isValid(value)
+    ) {
+      setCreditNumber(value);
+      return true;
+    } else {
+      return false;
+    }
+  };
 
-const valid=()=>{
-let expirationMonth=expiration.slice(0,2)
-console.log(expirationMonth)
-let expirationYear=expiration.slice(2,4)
-console.log(expirationYear)
-  if(validateCreditCardNumber(creditNumber) && isSecurityCodeValid(creditNumber,cvc) &&(isExpirationDateValid(expirationMonth,expirationYear))){
-  setSuccess("Your order has been carried out successfully")
-  setError("")
- 
-    Swal.fire({
-    icon:'success',
-    iconColor:'rgba(254, 215, 102, 1)',
-    title:'Thanks for your purchase',
-    background:'#E5E5E5',
-    timer: 3000,
-    showConfirmButton:false,
-    color:'rgba(54, 53, 55, 1)'
-      }
-      )
-  
-  setTimeout(()=>{
-    history('/')
-  },5000)
-}
-else{
-  setError("Please, enter a valid credit card")
-}}
+  const valid = () => {
+    let expirationMonth = expiration.slice(0, 2);
+    let expirationYear = expiration.slice(3, 5);
+    if (
+      validateCreditCardNumber(creditNumber) &&
+      isSecurityCodeValid(creditNumber, cvc) &&
+      isExpirationDateValid(expirationMonth, expirationYear)
+    ) {
+      setError("");
+      let newOrder = createOrder();
+      localStorage.setItem("orders", JSON.stringify(newOrder));
 
-  const cashout=(e)=>{
-    e.preventDefault()
-    valid()
+      Swal.fire({
+        icon: "success",
+        iconColor: "rgba(254, 215, 102, 1)",
+        title: "Thanks for your purchase",
+        background: "#E5E5E5",
+        timer: 3000,
+        showConfirmButton: false,
+        color: "rgba(54, 53, 55, 1)",
+      });
+      setTimeout(() => {
+        history("/");
+      }, 5000);
+    } else {
+      setError("Please, enter a valid credit card");
+    }
+  };
 
-}
+  const cashout = (e) => {
+    e.preventDefault();
+    valid();
+  };
   return (
-    <div className='payform'>
-    {success ? <div><span>{success}</span></div> : <span></span>}
-    {error ? <div><span>{error}</span></div>: <div></div>}
-    <form className='form' onSubmit={cashout}>
+    <div className="payform">
+      {error ? <span className="error">*{error}</span> : <div></div>}
+      <form className="form" onSubmit={cashout}>
+        <label for="cardNumber">Card Number</label>
+        <div className="cardInp">
+          <InputMask
+            id="cardNumber"
+            alwaysShowMask="true"
+            mask="9999 9999 9999 9999"
+            onChange={(e) => validateCreditCardNumber(e.target.value)}
+          />
+          {creditLogo ? (
+            <img className="logo" src={creditLogo} alt={creditCard} />
+          ) : (
+            <img className="logo" src={creditcard} alt={creditCard} />
+          )}
+        </div>
+        {errorMsg ? <span>{errorMsg}</span> : <span></span>}
 
-  
-    <label for="cardNumber">Card Number</label>
-   <div className='cardInp'><input type="text" maxLength="16" placeholder="0000 - 0000 - 0000 - 0000" id="cardNumber" onChange={(e)=>validateCreditCardNumber(e.target.value)} />{creditLogo? <img className="logo" src={creditLogo} alt={creditCard}/> : <img className="logo" src={creditcard} alt={creditCard}/>}</div> 
-    {errorMsg? <span>{errorMsg}</span>:<span></span>}
- 
- 
-    <label className ="labelInp" for="expirationDate">MM/YY</label>
- <div className='cardInp'><input type="text" maxlength="4"  placeholder="00/00" id="expirationMonth" onChange={e=>setExpiration(e.target.value)}/>  </div>
+        <label className="labelInp" for="expirationDate">
+          MM/YY
+        </label>
+        <div className="cardInp">
+          <InputMask
+            mask="99/99"
+            alwaysShowMask="true"
+            id="expirationMonth"
+            onChange={(e) => setExpiration(e.target.value)}
+          />{" "}
+        </div>
 
+        <label className="labelInp" for="code">
+          CVC Code
+        </label>
+        <div className="cardInp">
+          <InputMask
+            alwaysShowMask="true"
+            mask="999"
+            id="code"
+            onChange={(e) => setCvc(e.target.value)}
+          />
+        </div>
 
-    <label className ="labelInp" for="code">CVC Code</label>
-    <div className='cardInp'><input  type="text" maxlength="3" placeholder="000"  id="code" onChange={e=>setCvc(e.target.value)}/></div>
-
-
-  <div >
-  <button type="submit" className="button" >pay now</button>
-  </div>
-</form>
-</div>
-  )
-}
+        <div>
+          <button type="submit" className="button">
+            pay now
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
